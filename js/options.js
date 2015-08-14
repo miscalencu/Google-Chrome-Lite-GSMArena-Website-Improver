fced27b4ac3bf907df06685c6d5924b3e53662e8
George Miscalencu <george.miscalencu@gmail.com>
2015-08-14 14:45:22 +0300

    Fix options page

---
+++ js/options.js
@@ -0,0 +1,6 @@
+﻿document.getElementById("highlightCompareDifferences").checked = (localStorage.highlightCompareDifferences == 1);
+//document.getElementById("useGallery").checked = (localStorage.useGallery == 1);
+
+document.getElementById("highlightCompareDifferences").onclick = function (e) { localStorage.highlightCompareDifferences = (this.checked ? 1 : 0); }
+//document.getElementById("useGallery").onclick = function (e) { localStorage.useGallery = (this.checked ? 1 : 0); }
+
