6dbc5461e42661cd3cdd7c8fcd9bcf06900e9a6d
George Miscalencu <george.miscalencu@gmail.com>
2016-02-28 10:51:27 +0200

    Added support for three (instead of only two) compared devices

--- js/main.js
+++ js/main.js
@@ -42,12 +42,14 @@ function fixvars(spec)
 	return spec;
 }
 
+/// returns 1, 2 or zero (if equals)
 function compareAsNumbers(var1, var2)
 {
 	var len1 = var1.length;
 	var len2 = var2.length;
 	var min = ((len1 < len2) ? len1 : len2);
 	var prevnumeric = 0;
+
 	for(var i = 0; i < min; i++)
 	{
 		if(var1[i] != var2[i])
@@ -56,7 +58,7 @@ function compareAsNumbers(var1, var2)
 			{
 				if(var1[i] != var2[i])
 				{
-					return ((var1[i] > var2[i])? 1 : 2);
+					return ((var1[i] > var2[i]) ? 1 : ((var1[i] < var2[i]) ? 2 : 0));
 				}
 				prevnumeric = 1;
 			}
@@ -84,6 +86,7 @@ function compareAsNumbers(var1, var2)
 						return 1;
 					}					
 				}
+
 				return 0;
 			}
 		}
@@ -98,32 +101,68 @@ function highlightDifferences() {
 		return;
 
 	var specsRows = specsTable.getElementsByTagName("tr");
-	var compare = 0;
+	var compare12 = 0, compare23 = 0, compare13 = 0;
+	var score1 = 0, score2 = 0, score3 = 0;
 
 	for (var ii = 0; ii < specsRows.length; ii++) {
 		var specCells = specsRows[ii].getElementsByTagName("td");
 		if (specCells.length == 4 || specCells.length == 5)
 		{
-			var pos1 = specCells.length - 2;
-			var pos2 = specCells.length - 3;
+			var pos1 = specCells.length - 3
+			var pos2 = specCells.length - 2;
+			var pos3 = specCells.length - 1;
 
 			var spec1 = fixvars(trim(specCells[pos1].innerText, "").replace(/\s+/g, ""));
 			var spec2 = fixvars(trim(specCells[pos2].innerText, "").replace(/\s+/g, ""));
+			var spec3 = fixvars(trim(specCells[pos3].innerText, "").replace(/\s+/g, ""));
 
-			if(spec1 != spec2)
+			if (spec2 == "" && spec3 == "") {
+				return;
+			}
+
+			if (spec1 != spec2 || (spec1 != spec3 && spec3 != "") || (spec2 != spec3 && spec3 != ""))
 			{
 				specCells[pos1].className += " gsmarena_improve_highlight";
 				specCells[pos2].className += " gsmarena_improve_highlight";
 
-				compare = compareAsNumbers(spec1, spec2);
-				if(compare == 1)
-					specCells[pos1].className += " gsmarena_improve_bold";
+				if (spec3 != "") {
+					specCells[pos3].className += " gsmarena_improve_highlight";
+				}
 
-				if(compare == 2)
-					specCells[pos2].className += " gsmarena_improve_bold";
+				// debugger;
+				// add 5 points for each winning position
+				// make them all red when no gray is required (there are two on the same position)
+				compare12 = compareAsNumbers(spec1, spec2);
+				if (spec3 == "") {
+					score1 = (compare12 == 1) ? 10 : 5;
+					score2 = (compare12 == 2) ? 10 : 5;
+				}
+				else {
+					compare23 = compareAsNumbers(spec2, spec3);
+					compare13 = compareAsNumbers(spec1, spec3);
+
+					score1 = (((compare12 == 1) ? 1 : 0) + ((compare13 == 1) ? 1 : 0)) * 5;
+					score2 = (((compare12 == 2) ? 1 : 0) + ((compare23 == 1) ? 1 : 0)) * 5;
+					score3 = (((compare13 == 2) ? 1 : 0) + ((compare23 == 2) ? 1 : 0)) * 5;
+
+					// when they are all equal
+					if (compare12 == 0 && compare13 == 0 && compare23 == 0) {
+						score1 = 5;
+						score2 = 5;
+						score3 = 5;
+					}
+				}
+
+				specCells[pos1].className += " gsmarena_improve_highlight" + score1;
+				specCells[pos2].className += " gsmarena_improve_highlight" + score2;
+
+				if (spec3 != "") {
+					specCells[pos3].className += " gsmarena_improve_highlight" + score3;
+				}
 
 				//specCells[pos1].innerHTML = spec1;
 				//specCells[pos2].innerHTML = spec2;
+				//specCells[pos3].innerHTML = spec3;
 			}
 		}
 	}
